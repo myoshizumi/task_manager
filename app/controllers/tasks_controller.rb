e609351e745a2e18a8535098520e91079374b783
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-02-16 00:13:07 +0900

    Create new page and edit page, including flash msg for validation errors

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,42 @@
 class TasksController < ApplicationController
+
   def index
+    @tasks = Task.all
   end
 
   def new
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to new_task_path, notice: "タスクを登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :detail)
+  end
 end

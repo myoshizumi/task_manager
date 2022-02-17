d7fa9d5ae786d0f751cebea45305383f89e60a7c
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-02-18 02:19:43 +0900

    Add system spec for descending order

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task){ FactoryBot.create(:task, name: 'task') }
+  before do
+    visit tasks_path
+  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-        task = FactoryBot.create(:task, name: 'task')
         visit new_task_path
-        fill_in 'Name', with: "Anything"
-        fill_in 'Detail', with: "NothingToDo"
+        fill_in 'task_name', with: "Anything"
+        fill_in 'task_detail', with: "NothingToDo"
         click_on 'commit'
         expect(page).to have_content 'task'
       end
     end
   end
+
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, name: 'task')
         visit tasks_path
         current_path
         Task.count
@@ -23,11 +26,18 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task'
       end
     end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        visit tasks_path
+        current_path
+        task_list = all('.task_row')
+        expect(task_list).to be {Task.all.order(updated_at: :desc)}
+      end
+    end
   end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-        task = FactoryBot.create(:task, name: 'task')
         visit task_path(task.id)
         current_path
         Task.count

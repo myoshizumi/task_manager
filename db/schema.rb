ce9bd82c0d73657d36385f5426e7c3ecd89408ee
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-02-17 19:04:18 +0900

    Add ordered by updated_at:desc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_16_155812) do
+ActiveRecord::Schema.define(version: 2022_02_17_090106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_02_16_155812) do
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "detail", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

f851da059fe588eb65de266cd279fe23720a2103
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-02-19 19:41:01 +0900

    Make it possible to search with vague letters

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,23 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
 
   def index
-    if params[:sort_expired]
+    @tasks = Task.all.order(created_at: :desc)
+    if params[:task].present?
+      @tasks = @tasks.where('name LIKE ?', "%#{params[:task][:search]}%") 
+    elsif params[:sort_expired]
       @tasks = Task.all.order(expired_at: :desc)
     else
       @tasks = Task.all.order(created_at: :desc)
     end
   end
+
+  # def task_name_search
+  #   if params[:task].present?
+  #     @tasks = Task.by_name_search
+  #   else
+  #     @tasks = Task.all.order(created_at: :desc)
+  #   end
+  # end
 
   def new
     @task = Task.new
@@ -47,7 +58,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :detail, :expired_at)
+    params.require(:task).permit(:name, :detail, :expired_at, :search)
   end
 
   def set_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :detail, presence: true
   # validates :expired_at, presence: true
+  # scope :by_name_search, => { where('name like ?', '%%')}
 end

3e55be040b020e16153471556b18d4a5c2e1670d
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-02-26 13:44:58 +0900

    Add error pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,15 @@ User.create(name: "No",
     email: "nomore@errors.com",
     password: "anymore",
     password_confirmation: "anymore",
-    admin: true)
+    admin: false)
 User.create(name: "Admin",
     email: "no@errors.com",
     password: "nomore",
     password_confirmation: "nomore",
     admin: false)
+User.create(name: "mine",
+    email: "no@more.com",
+    password: "noerror",
+    password_confirmation: "noerror",
+    admin: true)
+

cfabfed9bccaf0fd6872a0edacb40a68e074a653
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-02-28 01:38:31 +0900

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,31 @@
 # coding: utf-8
-
-User.create(name: "Admin",
-    email: "none@errors.com",
-    password: "nomore",
-    password_confirmation: "nomore",
-    admin: false)
+10.times do |n|
+    User.create(name: "Admin",
+        email: "none#{n+50}@errors.com",
+        password: "nomore",
+        password_confirmation: "nomore",
+        admin: false)
+    end
 User.create(name: "mine",
-    email: "noneno@more.com",
+    email: "nonenone@more.com",
     password: "noerror",
     password_confirmation: "noerror",
     admin: false)
 
-3.times do |i|
-    Task.create(user_id: User.find(i+1).id, 
-    name: "Task #{i+1}", 
-    detail: "Detail #{i+1}",
-    expired_at: '01/01')
+
+User.all.each do |user|
+    10.times do |n|
+    user.tasks.create!(user_id: User.ids, 
+    name: "Task", 
+    detail: "Detail",
+    expired_at: '01/01'
+    label: )
+    end
 end
 
     
-# Label.create(name: "Work")
-# Label.create(name: "Private")
-# Label.create(name: "School")
-# Label.create(name: "Community")
-# Label.create(name: "Family")
+Label.create(name: "Work")
+Label.create(name: "Private")
+Label.create(name: "School")
+Label.create(name: "Community")
+Label.create(name: "Family")

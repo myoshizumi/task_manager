2501eaccc0d6ff65d5e0026883c3b5c7c4ff4495
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-03-21 12:00:13 +0900

    TEMPORARY FIX for logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root to: 'users#new'
   resources :tasks
   resources :sessions, only: %i[new create destroy]
-
   resources :users, only: %i[new create show edit update]
   namespace :admin do
     resources :users
